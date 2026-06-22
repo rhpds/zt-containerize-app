20c50e5af368aa3dda7cec0240d2e70b6506b8fc
baraka <126705717+Brillar0101@users.noreply.github.com>
2026-06-22 10:23:18 -0400

    Unregister and re-register the system

--- setup-automation/setup-rhel.sh
+++ setup-automation/setup-rhel.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 # Clone the app repo to the user directory
+# Unregister and register the VM
+subscription-manager clean
+subscription-manager register --activationkey=12-5-22-instruqt --org=12451665 --force
+
 su - rhel -c "git clone https://github.com/ellisonleao/clumsy-bird"
 
 echo "DONE" >> /root/post-run.log
