5e766c9ba48835b8c034ff108d2188220ee60f0a
barakaeli <126705717+Brillar0101@users.noreply.github.com>
2026-07-06 11:15:15 -0400

    Add podman installation

--- setup-automation/setup-rhel.sh
+++ setup-automation/setup-rhel.sh
@@ -5,7 +5,7 @@ subscription-manager clean
 subscription-manager register --activationkey=12-5-22-instruqt --org=12451665 --force
 
 # Install packages 
-sudo dnf install -y buildah
+sudo dnf install -y buildah podman
 
 # Clone the app repo to the user directory
 su - rhel -c "git clone https://github.com/ellisonleao/clumsy-bird"
