05b1c7b3871e2aa6a1ce50aae0219fa30f471a86
Wilson Harris <wharris@redhat.com>
2026-07-07 09:54:26 -0400

    Update setup-script for system registration

--- setup-automation/setup-rhel.sh
+++ setup-automation/setup-rhel.sh
@@ -3,6 +3,7 @@
 # Clone the app repo to the user directory
 su - rhel -c "git clone https://github.com/ellisonleao/clumsy-bird"
 
+dnf -y remove katello-ca-consumer-*
 subscription-manager clean
 subscription-manager register --activationkey=$ACTIVATION_KEY --org=$ORG_ID
 
