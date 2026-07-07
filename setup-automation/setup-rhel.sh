ce775aa7e292b7098caf10f0d2ee7bd330457587
Wilson Harris <wharris@redhat.com>
2026-07-07 09:37:15 -0400

    Testing vaulted AK and Org ID

--- setup-automation/setup-rhel.sh
+++ setup-automation/setup-rhel.sh
@@ -3,4 +3,9 @@
 # Clone the app repo to the user directory
 su - rhel -c "git clone https://github.com/ellisonleao/clumsy-bird"
 
+subscription-manager clean
+subscription-manager register --activationkey=$ACTIVATION_KEY --org=$ORG_ID
+
+dnf install -y buildah
+
 echo "DONE" >> /root/post-run.log
